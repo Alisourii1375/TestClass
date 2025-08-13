328359a8eb1c809bc91ce70c0da2a346e1d3dc7d
Ali Souri <alisourii1375@gmail.com>
2025-08-13 01:54:47 -0700

    Add project files.

---
+++ TestClass/NewClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClass
+{
+    public class Multi
+    {
+        public int mult(int a , int b)
+        {
+
+            return (a * b);         
+        }
+
+        public decimal dmult(decimal a, decimal b)
+        {
+
+            return (a * b);
+        }
+
+        public int  threemult( int a, int  b, int  c)
+        {
+
+            return (a * b  * c ) ;
+        }
+
+        public decimal fourmult(decimal a, decimal b, decimal c , decimal d )
+        {
+
+            return (a * b * c * d );
+        }
+
+
+
+    }
+}
---
+++ TestClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArghamNegarLibrary; 
+ 
+
+namespace TestClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Multi m  = new Multi();
+
+            Console.WriteLine( m.mult(2, 3));
+            Console.WriteLine( m.threemult(2, 3 , 4 ));
+            Console.WriteLine( m.fourmult(Convert.ToDecimal( 3.1416)
+                        , Convert.ToDecimal(3.3575)
+                        , Convert.ToDecimal(4.575)
+                        , Convert.ToDecimal(5.57578)
+                        )
+                )
+                        ;
+
+            
+        }
+    }
+}
